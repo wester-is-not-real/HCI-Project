4db18fb1990a557a8ff511960f0bd75cc2816e65
wester-is-not-real <wester.aldarondo@upr.edu>
2024-04-15 11:00:56 -0400

    Added Tailwind CSS and DaisyUI

--- tune-twins/capacitor.config.ts
+++ tune-twins/capacitor.config.ts
@@ -3,7 +3,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.tunetwins.app',
   appName: 'tunetwins',
-  webDir: 'dist',
+  webDir: 'build',
   server: {
     androidScheme: 'https'
   }
